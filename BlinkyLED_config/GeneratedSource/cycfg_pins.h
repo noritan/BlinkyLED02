089cd4979ad801665389a80a6733c6e0a93dca2e
noritan <github@noritan.org>
2019-02-12 14:42:51 +0900

    Relocated Pin_LEDR position
    Relocated Pin_LEDR from P6[3] to P0[3]
    Debug attributes added

--- BlinkyLED_config/GeneratedSource/cycfg_pins.h
+++ BlinkyLED_config/GeneratedSource/cycfg_pins.h
@@ -24,16 +24,16 @@
 extern "C" {
 #endif
 
-#define Pin_LEDR_PORT GPIO_PRT6
+#define Pin_LEDR_PORT GPIO_PRT0
 #define Pin_LEDR_PIN 3U
 #define Pin_LEDR_NUM 3U
 #define Pin_LEDR_DRIVEMODE CY_GPIO_DM_STRONG_IN_OFF
 #define Pin_LEDR_INIT_DRIVESTATE 1
-#ifndef ioss_0_port_6_pin_3_HSIOM
-	#define ioss_0_port_6_pin_3_HSIOM HSIOM_SEL_GPIO
+#ifndef ioss_0_port_0_pin_3_HSIOM
+	#define ioss_0_port_0_pin_3_HSIOM HSIOM_SEL_GPIO
 #endif
-#define Pin_LEDR_HSIOM ioss_0_port_6_pin_3_HSIOM
-#define Pin_LEDR_IRQ ioss_interrupts_gpio_6_IRQn
+#define Pin_LEDR_HSIOM ioss_0_port_0_pin_3_HSIOM
+#define Pin_LEDR_IRQ ioss_interrupts_gpio_0_IRQn
 #define SWO_PORT GPIO_PRT6
 #define SWO_PIN 4U
 #define SWO_NUM 4U
